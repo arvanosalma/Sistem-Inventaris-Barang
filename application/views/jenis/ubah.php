68e9c67920f1ca89ce741ec6af8d4bf3a394ccde
arvanosalma <91399210+arvanosalma@users.noreply.github.com>
2021-11-25 18:49:47 +0700

    Update ubah.php

--- application/views/jenis/ubah.php
+++ application/views/jenis/ubah.php
@@ -9,8 +9,8 @@
             <form action="" method="post">
                 <input type="hidden" name="id_jenis" id="id_jenis" value="<?= $jenis['id_jenis']; ?>">
                 <div class="mb-3">
-                    <label for="nama_jenis" class="formlabel">Nama Jenis</label>
-                    <input type="text" class="formcontrol" name="nama_jenis" id="nama_jenis" value="<?= $jenis['nama_jenis']; ?>">
+                    <label for="nama_jenis" class="form-label">Nama Jenis</label>
+                    <input type="text" class="form-control" name="nama_jenis" id="nama_jenis" value="<?= $jenis['nama_jenis']; ?>">
                 </div>
                 <button name="ubah" type="submit" class="btn btn-primary">Ubah Data</button>
             </form>
