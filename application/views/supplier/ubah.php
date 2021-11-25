3c443d21f21fe947ea0a4d8d1256259208c6556c
arvanosalma <91399210+arvanosalma@users.noreply.github.com>
2021-11-25 19:00:57 +0700

    Update ubah.php

--- application/views/supplier/ubah.php
+++ application/views/supplier/ubah.php
@@ -9,16 +9,16 @@
             <form action="" method="post">
                 <input type="hidden" name="id_supplier" id="id_supplier" value="<?= $supplier['id_supplier']; ?>">
                 <div class="mb-3">
-                    <label for="nama_supplier" class="formlabel">Nama Supplier</label>
-                    <input type="text" class="formcontrol" name="nama_supplier" id="nama_supplier" value="<?= $supplier['nama_supplier']; ?>">
+                    <label for="nama_supplier" class="form-label">Nama Supplier</label>
+                    <input type="text" class="form-control" name="nama_supplier" id="nama_supplier" value="<?= $supplier['nama_supplier']; ?>">
                 </div>
                 <div class="mb-3">
-                    <label for="no_telp" class="formlabel">No Telepon Supplier</label>
-                    <input type="text" class="formcontrol" name="no_telp" id="no_telp" value="<?= $supplier['no_telp']; ?>">
+                    <label for="no_telp" class="form-label">No Telepon Supplier</label>
+                    <input type="text" class="form-control" name="no_telp" id="no_telp" value="<?= $supplier['no_telp']; ?>">
                 </div>
                 <div class="mb-3">
-                    <label for="alamat" class="formlabel">Alamat Supplier</label>
-                    <input type="text" class="formcontrol" name="alamat" id="alamat" value="<?= $supplier['alamat']; ?>">
+                    <label for="alamat" class="form-label">Alamat Supplier</label>
+                    <input type="text" class="form-control" name="alamat" id="alamat" value="<?= $supplier['alamat']; ?>">
                 </div>
                 <button name="ubah" type="submit" class="btn btn-primary">Ubah Data</button>
             </form>
